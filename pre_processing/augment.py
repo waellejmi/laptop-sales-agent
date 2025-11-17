c82fc93323a4155248c33f4b826c177d380f55eb
waellejmi <wael.lejmi02@gmail.com>
2025-11-17 21:07:21 +0100

    Planning

---
+++ pre_processing/augment.py
@@ -0,0 +1,2 @@
+# TODO: change rupees to euro, standarize os(windows/linux/macos), add resolution_category (FHD, HD, 4K)
+# model_name, brand, cpu, cpu_cores, cpu_threads, ram_gb, ssd_gb, hdd_gb, os, gpu, screen_size_in, resolution_w, resolution_h, resolution_type, spec_score, price_euro
